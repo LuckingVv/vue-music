e9998029a6efa6e7580b06b1994ca3013de74cbe
WangAnWei <william_waw@163.com>
2019-02-14 17:59:52 +0800

    1

--- src/base/loading/loading.vue
+++ src/base/loading/loading.vue
@@ -1,28 +1,31 @@
 <template>
-    <div class="loading">
-        <img src="./loading.gif" width="40" height="40">
-        <p class="desc">{{title}}</p>
-    </div>
+  <div class="loading">
+    <img src="./loading.gif" width="40" height="40">
+    <p class="desc">{{title}}</p>
+  </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 export default {
-    props: {
-        title: {
-            type: String,
-            default: '正在载入...'
-        }
+  props: {
+    title: {
+      type: String,
+      default: '正在载入...'
     }
+  }
 }
 </script>
 <style lang="stylus" scoped>
-    @import '~common/stylus/variable'
+@import '~common/stylus/variable'
 
-    .loading
-        width: 100%
-        text-align: center
-        .desc
-            line-height: 20px
-            font-size: $font-size-small
-            color: $color-text-l
+.loading {
+  width: 100%
+  text-align: center
+
+  .desc {
+    line-height: 20px
+    font-size: $font-size-small
+    color: $color-text-l
+  }
+}
 </style>
